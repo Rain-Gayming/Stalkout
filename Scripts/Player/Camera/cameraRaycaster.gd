ed5cb67638c9e60f37fb3d93b813971aa2cc2f0f
Rain-Gayming <raingayming@gmail.com>
2024-02-26 20:56:52 +0000

    Interaction performance improvement

--- Scripts/Player/Camera/cameraRaycaster.gd
+++ Scripts/Player/Camera/cameraRaycaster.gd
@@ -2,11 +2,11 @@ extends RayCast3D
 
 func _process(delta):
 	#checks if the raycast hits something
-	if is_colliding():
-		#script on object
-		var objectScript = get_collider().get_script()
-		
-		#if interact key is pressed pick up item
-		if Input.is_action_just_pressed("interact"):
+	if Input.is_action_just_pressed("interact"):
+		if is_colliding(): 
+			#script on object
+			var objectScript = get_collider().get_script()
+			
+			#if interact key is pressed pick up item
 			if objectScript == groundItem:
 				objectScript.interact()
